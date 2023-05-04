dcd7bfc90df51ac99c0782faf20ab91d82591cc8
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-04 15:41:09 +0200

    refactor: use traits to manage dependancies

--- src/main/scala/field/Field.scala
+++ src/main/scala/field/Field.scala
@@ -3,23 +3,31 @@ package field
 import field.GlobalField
 import field.defaultable.Defaultable
 
-/**
- * A field is a map from device ids to values of type A.
- * When a device is not in the map, the default value is used.
- * @tparam A the type of the field
- */
-trait Field[A] extends GlobalField[A] with Defaultable[A]
-object Field:
-  def apply[A](m: Map[DeviceId, A], d: A): Field[A] = new Field[A]:
-    def getMap: Map[DeviceId, A] = m
-    def default: A = d
 
-  def apply[A](m: Map[DeviceId, A])(using defaultable: Defaultable[A]): Field[A] = new Field[A]:
-    def getMap: Map[DeviceId, A] = m
-    def default: A = defaultable.default
-  
-  def lift[A](a: A): Field[A] = new Field[A]:
-    def getMap: Map[DeviceId, A] = Map.empty
-    def default: A = a
+trait Fields:
+  /**
+   * A field is a map from device ids to values of type A.
+   * When a device is not in the map, the default value is used.
+   *
+   * @tparam A the type of the field
+   */
+  trait Field[A] extends GlobalField[A] with Defaultable[A]
 
-  def changeDef[A](field: Field[A], newDefault: A): Field[A] = Field(field.getMap, newDefault)
+  object Field:
+    def apply[A](m: Map[DeviceId, A], d: A): Field[A] = new Field[A]:
+      def getMap: Map[DeviceId, A] = m
+
+      def default: A = d
+
+    def apply[A](m: Map[DeviceId, A])(using defaultable: Defaultable[A]): Field[A] = new Field[A]:
+      def getMap: Map[DeviceId, A] = m
+
+      def default: A = defaultable.default
+
+    def lift[A](a: A): Field[A] = new Field[A]:
+      def getMap: Map[DeviceId, A] = Map.empty
+
+      def default: A = a
+
+    def changeDef[A](field: Field[A], newDefault: A): Field[A] = Field(field.getMap, newDefault)
+
--- src/main/scala/field/monad/Monad.scala
+++ src/main/scala/field/monad/Monad.scala
@@ -1,20 +1,24 @@
 package field.monad
 
-/**
- * Monad Type class
- * @tparam F the type of the monad
- */
-trait Monad[F[_]]:
-  def unit[A](a: => A): F[A]
-  extension [A] (fa: F[A])
-    def flatMap[B](f: A => F[B]): F[B]
+import field.Fields
 
-    def map[B](f: A => B): F[B] =
-      fa.flatMap(a => unit(f(a)))
+trait Monads:
+  /**
+   * Monad Type class
+   *
+   * @tparam F the type of the monad
+   */
+  trait Monad[F[_]]:
+    def unit[A](a: => A): F[A]
 
-object FieldMonad:
-  import field.Field
+    extension[A] (fa: F[A])
+      def flatMap[B](f: A => F[B]): F[B]
 
+      def map[B](f: A => B): F[B] =
+        fa.flatMap(a => unit(f(a)))
+
+
+trait MonadicFields extends Monads with Fields:
   /**
    * Field Monad instance
    */
@@ -22,9 +26,10 @@ object FieldMonad:
     def unit[A](a: => A): Field[A] =
       Field.lift(a)
 
-    extension [A](fa: Field[A])
+    extension[A] (fa: Field[A])
       def flatMap[B](f: A => Field[B]): Field[B] =
         Field(fa.getMap.map { case (id, a) => id -> {
           val b = f(a)
           b.getMap.getOrElse(id, b.default)
-        } }, f(fa.default).default)
+        }
+        }, f(fa.default).default)
