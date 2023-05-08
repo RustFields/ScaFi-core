d260ad9d6739f08b2220735d48d9f02aed0d116e
paolo <paolo.penazzi@studio.unibo.it>
2023-05-08 13:58:29 +0200

    docs: add documentation to Path class

--- src/main/scala/Path.scala
+++ src/main/scala/Path.scala
@@ -1,10 +1,51 @@
+/**
+ * A path is a list of slots that represent a path in a tree.
+ * The path works like a stack.
+ */
 trait Path:
+
+  /**
+   * Pushes a slot to the path
+   * @param slot the slot to push
+   * @return the new path
+   */
   def push(slot: Slot): Path
+
+  /**
+   * Pulls the last slot from the top of the path
+   * @return the new path
+   */
   def pull(): Path
+
+  /**
+   * Checks if the path is equal to another path
+   * @param path the path to check
+   * @return true if the paths are equal, false otherwise
+   */
   def isEqualTo(path: Path): Boolean
+
+  /**
+   * Checks if the path is the root path
+   * @return true if the path is the root path, false otherwise
+   */
   def isRoot: Boolean
+
+  /**
+   * Returns the head of the path (the last slot pushed)
+   * @return the head of the path
+   */
   def head: Slot
+
+  /**
+   * @return the path as a list of slots
+   */
   def path: List[Slot]
+
+  /**
+   * Pushes a slot to the path
+   * @param slot the slot to push
+   * @return the new path
+   */
   def /(slot: Slot): Path = push(slot)
 
 object Path:
@@ -14,7 +55,7 @@ object Path:
 
     override def push(slot: Slot): Path = PathImpl(slot :: path)
 
-    def pull(): Path = path match
+    override def pull(): Path = path match
       case _ :: p => PathImpl(p)
       case _ => throw new Exception()
 
