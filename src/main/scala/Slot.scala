eee8f73bd053525397e8a6202270a3e6b6131f9e
paolo <paolo.penazzi@studio.unibo.it>
2023-05-08 14:10:05 +0200

    feat: add missing methods to Slot trait

--- src/main/scala/Slot.scala
+++ src/main/scala/Slot.scala
@@ -1,4 +1,17 @@
-trait Slot
+trait Slot:
+  /**
+   * This method is used to associate a value to a path.
+   * @param value the value to be associated with the path
+   * @return a tuple containing a path and the corresponding value
+   */
+  def -> (value: Any): (Path, Any) = (Path(this), value)
+
+  /**
+   * Creates a path by concatenating this slot with the given slot.
+   * @param slot the slot to be concatenated
+   * @return a path
+   */
+  def /(slot: Slot): Path = Path(this, slot)
 
 object Slot:
   final case class Nbr(index: Int) extends Slot
