8e85efeaed2843ece661f694935ad81fd705f178
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-10 11:36:38 +0200

    feat: add auxiliary constructs to field lang

--- src/main/scala/lang/FieldLang.scala
+++ src/main/scala/lang/FieldLang.scala
@@ -2,7 +2,7 @@ package lang
 
 import field.Fields
 
-trait FieldLang extends Language with Fields:
+trait FieldLang extends Language with Fields with AuxiliaryConstructs:
   override type F[A] = Field[A]
 
   override def rep[A](init: => Field[A])(fun: Field[A] => Field[A]): Field[A]
