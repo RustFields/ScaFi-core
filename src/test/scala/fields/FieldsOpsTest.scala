8c0c46bbb5d381542ef3fad009a4b0404c54b243
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-10 14:39:40 +0200

    feat: add test case for comprehensions

--- src/test/scala/fields/FieldsOpsTest.scala
+++ src/test/scala/fields/FieldsOpsTest.scala
@@ -11,3 +11,13 @@ class FieldsOpsTest extends AnyFlatSpec with FieldTest with MonadicFields:
     val f = Field(Map(mid -> 1, "d2" ->1), 0)
     f.map(_ + 1).getMap shouldBe Map(mid -> 2, "d2" ->2)
   }
+
+  "Fields" should "be able to be used inside comprehensions" in {
+    val f = Field(Map(mid -> 1, "d2" ->1), 0)
+    val g = Field(Map(mid -> 2, "d2" ->2), 0)
+    val h = for {
+      x <- f
+      y <- g
+    } yield x + y
+    h.getMap shouldBe Map(mid -> 3, "d2" ->3)
+  }
