0d0666479626ccf8cbd226d61ef37395acdd3149
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-11 13:53:24 +0200

    test: added test case for field of function and values composition

--- src/test/scala/field/FieldsOpsTest.scala
+++ src/test/scala/field/FieldsOpsTest.scala
@@ -22,3 +22,16 @@ class FieldsOpsTest extends AnyFlatSpec with FieldTest with MonadicFields:
     } yield x + y
     h.getMap shouldBe Map(mid -> 3, "d2" ->3)
   }
+
+  "Fields" should "be composed in a functional way" in {
+    val plusOne = (x: Int) => x + 1
+    val plusTwo = (x: Int) => x + 2
+
+    val f = Field(Map(mid -> 1, "d2" ->2, "d3" -> 1, "d4" -> 2, "d5" -> 1))
+    val g: Field[Int => Int] = Field(Map(mid -> plusOne, "d2" -> plusTwo, "d3" -> plusOne, "d4" -> plusTwo, "d5" -> plusOne), (_:Int) => 0)
+    val h = for
+      x <- f
+      y <- g
+    yield y.apply(x)
+    h.getMap shouldBe Map(mid -> 2, "d2" -> 4, "d3" -> 2, "d4" -> 4, "d5" -> 2)
+  }
