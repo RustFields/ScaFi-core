6b72b7044bee30d1da95d7822be6924a585eaf6c
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-12 16:33:00 +0200

    feat: extend MonadicFields with field composition

--- src/main/scala/field/MonadicFields.scala
+++ src/main/scala/field/MonadicFields.scala
@@ -1,6 +1,7 @@
 package field
 
 import functional.monad.Monads
+import functional.monoid.Monoids.*
 import lang.AuxiliaryConstructs
 
 trait MonadicFields extends Monads with Fields with AuxiliaryConstructs:
@@ -20,3 +21,21 @@ trait MonadicFields extends Monads with Fields with AuxiliaryConstructs:
         }, f(fa.default).default)
 
 
+trait FieldOps extends MonadicFields:
+  given[A]: Conversion[A => A, Field[A => A]] with
+    override def apply(x: A => A): Field[A => A] = Field.lift(x)
+
+  def combineFields[A: Monoid](f1: Field[A], f2: Field[A]): Field[A] =
+    for
+      v1 <- f1
+      v2 <- f2
+    yield v1 |+| v2
+
+  def combineFields[A](f1: Field[A], f2: Field[A => A]): Field[A] =
+    for
+      v1 <- f1
+      v2 <- f2
+    yield v2(v1)
+
+  def applyToAll[A](f: Field[A], func: A => A): Field[A] =
+    combineFields(f, func)
