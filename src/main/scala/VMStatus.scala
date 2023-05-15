4b3e276647c2dd350abeebf2211f3889c6d52524
paolo <paolo.penazzi@studio.unibo.it>
2023-05-15 11:46:25 +0200

    chore: text formatting

--- src/main/scala/VMStatus.scala
+++ src/main/scala/VMStatus.scala
@@ -4,30 +4,35 @@
 trait VMStatus:
   /**
    * The path of the computation.
+   *
    * @return the current path
    */
   def path: Path
 
   /**
    * The index of the current slot.
+   *
    * @return the index
    */
   def index: Int
 
   /**
    * The id of the current neighbour. If the current slot is not a folding slot, this value is None.
+   *
    * @return the id
    */
   def neighbour: Option[Int]
 
   /**
    * Whether the VM is folding or not.
+   *
    * @return true if the VM is folding, false otherwise
    */
   def isFolding: Boolean
 
   /**
    * Fold the current slot into the given neighbour.
+   *
    * @param id the id of the neighbour
    * @return the new status
    */
@@ -35,12 +40,14 @@ trait VMStatus:
 
   /**
    * Fold out of the current slot.
+   *
    * @return the new status
    */
   def foldOut(): VMStatus
 
   /**
    * Nest the given slot.
+   *
    * @param s the slot to nest
    * @return the new status
    */
@@ -48,24 +55,28 @@ trait VMStatus:
 
   /**
    * Increment the index of the current slot.
+   *
    * @return the new status
    */
   def incIndex(): VMStatus
 
   /**
    * Push the current status on the stack.
+   *
    * @return the new status
    */
   def push(): VMStatus
 
   /**
    * Pop the current status from the stack.
+   *
    * @return the new status
    */
   def pop(): VMStatus
 
 object VMStatus:
   def apply(path: Path): VMStatus = VMStatusImpl(path)
+
   def apply(): VMStatus = VMStatusImpl()
 
   final private case class VMStatusImpl(override val path: Path = Path(),
@@ -75,11 +86,17 @@ object VMStatus:
                                        ) extends VMStatus:
 
     override def isFolding: Boolean = neighbour.isDefined
+
     override def foldInto(id: Option[Int]): VMStatus = VMStatusImpl(path, index, id, stack)
+
     override def foldOut(): VMStatus = VMStatusImpl(path, index, None, stack)
+
     override def nest(s: Slot): VMStatus = VMStatusImpl(path.push(s), 0, neighbour, stack)
+
     override def incIndex(): VMStatus = VMStatusImpl(path, index + 1, neighbour, stack)
+
     override def push(): VMStatus = VMStatusImpl(path, index, neighbour, (path, index, neighbour) :: stack)
+
     override def pop(): VMStatus = stack match
       case (p, i, n) :: s => VMStatusImpl(p, i, n, s)
       case _ => throw new Exception()
