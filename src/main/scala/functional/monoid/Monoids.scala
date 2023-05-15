404bbe42604f0707b2150ea6492104201b423636
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-15 11:30:44 +0200

    refactor: made monoid instances for cats.Monoid

--- src/main/scala/functional/monoid/Monoids.scala
+++ src/main/scala/functional/monoid/Monoids.scala
@@ -1,25 +1,24 @@
 package functional.monoid
 
+import cats.kernel.Monoid
+
 object Monoids:
-  trait Monoid[A]:
-    def op(a1: A, a2: A): A
-    def zero: A
 
   extension [A: Monoid](a1: A)
-    def |+|(a2: A): A = summon[Monoid[A]].op(a1, a2)
+    def |+|(a2: A): A = summon[Monoid[A]].combine(a1, a2)
 
   given Monoid[Int] with
-    def op(a1: Int, a2: Int): Int = a1 + a2
-    def zero: Int = 0
+    def combine(a1: Int, a2: Int): Int = a1 + a2
+    def empty: Int = 0
 
   given Monoid[String] with
-    def op(a1: String, a2: String): String = a1 + a2
-    def zero: String = ""
+    def combine(a1: String, a2: String): String = a1 + a2
+    def empty: String = ""
 
   given[A]: Monoid[List[A]] with
-    def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
-    def zero: List[A] = Nil
+    def combine(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+    def empty: List[A] = Nil
 
   given[A]: Monoid[Set[A]] with
-    def op(a1: Set[A], a2: Set[A]): Set[A] = a1 ++ a2
-    def zero: Set[A] = Set.empty
+    def combine(a1: Set[A], a2: Set[A]): Set[A] = a1 ++ a2
+    def empty: Set[A] = Set.empty
