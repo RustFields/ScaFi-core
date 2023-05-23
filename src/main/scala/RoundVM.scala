2f0d161ad07ca19da9f280ee9336732eb2763b92
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-05-23 16:05:44 +0200

    feat: complete roundVM api definition

--- src/main/scala/RoundVM.scala
+++ src/main/scala/RoundVM.scala
@@ -1,3 +1,6 @@
+import Path.*
+import RoundVM.{NbrSensorUnknownException, OutOfDomainException, SensorUnknownException}
+
 trait RoundVM:
   /**
    * The context of the current round
@@ -39,7 +42,7 @@ trait RoundVM:
    *
    * @return the id
    */
-  def neighbor: Option[Int] = status.neighbour
+  def neighbour: Option[Int] = status.neighbour
 
   /**
    * The index of the current computation
@@ -48,25 +51,74 @@ trait RoundVM:
    */
   def index: Int = status.index
 
-  /**
-   * Whether the device is contained in the neighbor list
-   *
-   * @return true if the device is contained in the neighbor list, false otherwise
-   */
-  def onlyWhenFoldingOnSelf: Boolean = neighbor.contains(selfID)
+
+  def exportStack: List[Export]
+
+  def self: Int =
+    context.selfID
+
+  def registerRoot(v: Any): Unit =
+    exportData.put(Path.empty(), v)
+
+  def previousRoundVal[A]: Option[A] =
+    context.readExportValue[A](self, status.path)
+
+  def neighbourVal[A]: A = context
+    .readExportValue[A](neighbour.get, status.path)
+    .getOrElse(throw OutOfDomainException(context.selfID, neighbour.get, status.path))
+
+  def localSense[A](name: Sensor): A = context
+    .localSense[A](name)
+    .getOrElse(throw new SensorUnknownException(self, name))
+
+  def neighbourSense[A](name: Sensor): A = {
+    RoundVM.ensure(neighbour.isDefined, "Neighbouring sensor must be queried in a nbr-dependent context.")
+    context
+      .nbrSense(name)(neighbour.get)
+      .getOrElse(throw NbrSensorUnknownException(self, name, neighbour.get))
+  }
+
+  def foldedEval[A](expr: => A)(id: Int): Option[A]
+
+  def nest[A](slot: Slot)(write: Boolean, inc: Boolean = true)(expr: => A): A
+
+  def locally[A](a: => A): A
+
+  def alignedNeighbours(): List[Int]
+
+  def isolate[A](expr: => A): A
+
+  def newExportStack: Any
+
+  def discardExport: Any
+
+  def mergeExport: Any
 
   /**
    * Whether the device is contained in the neighbor list
    *
    * @return true if the device is contained in the neighbor list, false otherwise
    */
-  def unlessFoldingOnOthers: Boolean = neighbor.contains(selfID)
+  def onlyWhenFoldingOnSelf: Boolean = neighbour.contains(selfID)
+
+  /**
+   * Whether the device is contained in the neighbor list
+   *
+   * @return true if the device is contained in the neighbor list, false otherwise
+   */
+  def unlessFoldingOnOthers: Boolean = neighbour.contains(selfID)
 
 object RoundVM:
-  def apply(context: Context,
-            exportsStack: List[Export] = List(Export.empty()),
-            status: VMStatus = VMStatus()): RoundVM = RoundVMImpl(context, status, exportsStack)
+  def ensure(b: => Boolean, s: String): Unit = if (!b) throw new Exception(s)
 
-  private case class RoundVMImpl(override val context: Context,
-                                 override val status: VMStatus,
-                                 override val exports: List[Export]) extends RoundVM
+  final case class OutOfDomainException(selfId: Int, nbr: Int, path: Path) extends Exception() {
+    override def toString: String = s"OutOfDomainException: $selfId , $nbr, $path"
+  }
+
+  final case class SensorUnknownException(selfId: Int, name: Sensor) extends Exception() {
+    override def toString: String = s"SensorUnknownException: $selfId , $name"
+  }
+
+  final case class NbrSensorUnknownException(selfId: Int, name: Sensor, nbr: Int) extends Exception() {
+    override def toString: String = s"NbrSensorUnknownException: $selfId , $name, $nbr"
+  }
