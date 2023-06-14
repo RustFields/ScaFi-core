209a8fa6497713341adcfb0dc490ffb8964c62b6
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-06-14 15:43:59 +0200

    docs: add language's doc

--- src/main/scala/lang/Language.scala
+++ src/main/scala/lang/Language.scala
@@ -4,18 +4,63 @@ import vm.{RoundVM, Sensor}
 import vm.Slot.{Branch, FoldHood, Nbr, Rep}
 
 trait Language:
+  /**
+   * Iteratively updates the value of the input expression at each device using the last computed value.
+   * @param init the initial value
+   * @param fun the function to apply to the value
+   * @tparam A
+   * @return the updated value
+   */
   def rep[A](init: => A)(fun: A => A): A
 
+  /**
+   * Observes the value of an expression across neighbors, producing a “field of fields”.
+   * @param expr the expression to evaluate
+   * @tparam A
+   * @return the value of the expression
+   */
   def nbr[A](expr: => A): A
 
+  /**
+   * The id of the local device
+   * @return the ID as Int
+   */
   def mid(): Int
 
+  /**
+   * Aggregates the results of the neighbor computation.
+   * @param init The initial value for the aggregation.
+   * @param aggr The aggregation function to combine values.
+   * @param expr The expression to evaluate for each neighbor.
+   * @tparam A
+   * @return The final result of the aggregation.
+   */
   def foldhood[A](init: => A)(aggr: (A, A) => A)(expr: => A): A
 
+  /**
+   * Partition the domain into two subspaces that do not interact with each others.
+   * @param cond the condition for the partition
+   * @param thn the function to apply if the condition is satisfied
+   * @param els the function to apply if the condition is *not* satisfied
+   * @tparam A
+   * @return the value of the function executed based on the condition
+   */
   def branch[A](cond: => Boolean)(thn: => A)(els: => A): A
 
+  /**
+   * Obtain the value of a local sensor.
+   * @param name the name of the sensor
+   * @tparam A
+   * @return the value of the local sensor
+   */
   def sense[A](name: Sensor): A
 
+  /**
+   * Obtain the value of the neighbor's sensor.
+   * @param name the name of the sensor
+   * @tparam A
+   * @return the value of the neighbor's sensor
+   */
   def nbrVar[A](name: Sensor): A
 
 
